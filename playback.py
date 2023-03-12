d2e6e73b8f8bd2b702fdc61daeff26bd41f024c1
Giorgi Abashidze <126471823+giorgi1121@users.noreply.github.com>
2023-03-12 23:01:23 +0400

    Create playback.py

---
+++ playback.py
@@ -0,0 +1,8 @@
+#create variable "text" which receive information from user
+text = input()
+
+#replace spaces from text with "..."
+replaced = text.replace(" ", "...")
+
+#print replaced text
+print(replaced)
